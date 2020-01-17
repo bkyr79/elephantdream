1d21af1eb3e32f9399ecc4ee8b1d059a0ae978df
takaomiyamashita <ty.8479@gmail.com>
2020-01-17 11:17:07 +0900

    create index file
    Fixed first view.

---
+++ elephantdream/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+<form method="POST" action="form2.php">
+<label>叶えたいことを入力してね</label><br/>
+<textarea name="desire"></textarea>
+<input type="submit" value="押す" onClick="location.href='/form2.php'"/>
+<input type="button" value="カレンダー" onClick="location.href='/calendar.php'"/>
+</form>
+</body>
+</html>
