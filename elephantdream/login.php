24a68208104f0315979432ef11de17ebcc15e896
takaomiyamashita <ty.8479@gmail.com>
2020-01-17 22:07:57 +0900

    index,loginを修正

--- elephantdream/login.php
+++ elephantdream/login.php
@@ -1,20 +1,36 @@
-<!-- <?php
-  include 'includes/login.php';
+<?php
   session_start();
-  if (!isset($_SESSION['id'])){
-    header('Location: login.php');
-    exit();
-  }
+  if (isset($_SESSION['id'])){
+    header('Location: index.php');
+  } else if (isset($_POST['name']) && isset($_POST['password'])){
+    $dsn = 'mysql:host=localhost;dbname=desire;charset'=utf8;
+    $user = 'desireuser';
+    $password = 'password';
+
+    try {
+      $db = new PDO($dsn, $user, $password);
+      $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+      $stmt = $db->prepare("
+        SELECT * FROM users WHERE name=:name AND password=:pass
+      ");
+
+      $stmt->bindParam(':name', $_POST['name'], PDO::PARAM_STR);
+      $stmt->bindParam(':pass', sha1($_POST['password']),PDO::PARAM_STR);
+      $stmt->execute();
+      if ($row = $stmt->fetch()){
+        $_SESSION['id'] = $row['id'];
+        header('Location: index.php');
+        exit();
+      } else {
+        header('Location: login.php');
+        exit();
+      }
+    } catch(PDOException $e) {
+      die ('エラー：' . $e->getMessage());
+    }
+  } else {
 ?>
- -->
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<title></title>
-</head>
-<body>
-<form method="POST" action="form4.php">
+
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -30,3 +46,4 @@
   </form>
 </body>
 </html>
+<?php } ?>
