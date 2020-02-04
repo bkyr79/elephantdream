0cf63daed0a36a4fe88fffb70896fcece5126b2d
takaomiyamashita <ty.8479@gmail.com>
2020-02-04 14:29:58 +0900

    Decorated login screen.
    form centered.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-  session_start();
+  // session_start();
 
   // define('DB_DATABASE', 'desire');
   // define('DB_USERNAME', 'desireuser');
@@ -39,19 +39,40 @@
   } else {
 ?>
 
+<!DOCTYPE html>
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <title>desire</title>
+  <style type="text/css">
+    body {
+      width: 400px;
+      height: 50px;
+      background-color: coral;
+      position: absolute;
+      top:200px;
+      right:0;
+      left:0;
+      margin:auto;
+    }
+    /* h2 { 
+    } */
+    p {
+      font-size: 20px;
+      font-family: "メイリオ";
+    }
+  </style>
 </head>
 <body>
   <h1></h1>
-  <h1>ログイン</h2>
+  <h2></h2>
   <form action="index.php" method="post">
-    <p>ユーザ名：<input type="text" name="name"></p>
-    <p>パスワード：<input type="password" name="password"></p>
+    <p>username：<input type="text" name="name"></p>
+    <p>password ：<input type="password" name="password"></p>
     <p><input type="submit" value="ログイン"></p>
   </form>
   </body>
+  
 </html>
 <?php } ?>
+
