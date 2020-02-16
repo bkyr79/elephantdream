1f98a7cdc8cfdb10cb8117bf897deb737cdfaccb
takaomiyamashita <ty.8479@gmail.com>
2020-02-16 17:43:05 +0900

    Create list.php

---
+++ list.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+  <title>やることリスト表示</title>
+</head>
+<body>
+  <h1>やることリスト</h1>
+  <p>やることリスト１:<?php echo $name; ?></p>
+</body>
+</html>
