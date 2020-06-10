e17021753dd09ab9c8485d600e6e24e58dd10654
takaomiyamashita <ty.8479@gmail.com>
2020-06-10 20:38:59 +0900

    [修正]全てのファイルを中央寄せ

--- conclusion.php
+++ conclusion.php
@@ -36,6 +36,73 @@
       a:visited { color: #0000CC; text-decoration: none; }
       a:hover   { color: rgb(29,161,242); text-decoration: underline; }
       a:active  { color: #0000cc; text-decoration: none; }
+
+      body {
+        background-image: url("/images/Visit pwdReykjavik iPhone Facebook Post.png");
+      }
+
+      .top {
+        width: 800px;
+        text-align: center;
+        margin: 100px 200px 0px;
+        padding: 100px 200px 0px 100px;
+      }
+
+      .bottom {
+        width: 800px;
+
+        text-align: center;
+        margin: 0px 50px 0px;
+      }
+
+      .container {
+        text-align: center;
+        padding: 0px 200px 0px;
+      }
+
+      .conclusion {
+        text-align: center;
+      }
+      .twitter {
+        font-weight: bold;
+        text-align: center;
+        height: 50px;
+        padding-top: 10px;
+        text-decoration: underline;
+      }
+
+      .twitter-share-button {
+        border: rgb(29,161,242);
+        color: rgb(29,161,242);
+        border-radius: 5px;
+        text-align: center;
+        font-weight: bold;
+        width: 205px;
+        height: 45px;
+        text-decoration: none;
+        /* text-decoration: underline; */
+      }
+
+      h3 {
+        font-size: 30px;
+      }
+
+      .kmain {
+        text-align: center;
+        margin: 100px 200px 0px;
+        padding: 50px 200px 0px;
+        border-radius: none;
+      }
+
+      table {
+        border: none;
+      }
+
+      .modoru {
+        text-align: center;
+        margin-left: 130px;
+        margin-top: 5px;
+      }
   </style>
   <div class="twitter">
   <a href="//twitter.com/share" class="twitter-share-button" data-text="↓今日から始めること↓" data-lang="ja">
--- form2.php
+++ form2.php
@@ -12,6 +12,75 @@
 <meta charset="utf-8">
 <link rel="stylesheet" type="text/css" href="stylesheet/form.css">
 <title></title>
+<style type="text/css">
+
+body {
+  background-image: url("/images/Visit pwdReykjavik iPhone Facebook Post.png");
+}
+
+.top {
+  width: 800px;
+  text-align: center;
+  margin: 100px 200px 0px;
+  padding: 100px 200px 0px 100px;
+}
+
+.bottom {
+  width: 800px;
+
+  text-align: center;
+  margin: 0px 50px 0px;
+}
+
+.container {
+  text-align: center;
+  padding: 0px 200px 0px;
+}
+
+.conclusion {
+  text-align: center;
+}
+.twitter {
+  font-weight: bold;
+  text-align: center;
+  height: 50px;
+  padding-top: 10px;
+  text-decoration: underline;
+}
+
+.twitter-share-button {
+  border: rgb(29,161,242);
+  color: rgb(29,161,242);
+  border-radius: 5px;
+  text-align: center;
+  font-weight: bold;
+  width: 205px;
+  height: 45px;
+  text-decoration: none;
+  /* text-decoration: underline; */
+}
+
+h3 {
+  font-size: 30px;
+}
+
+.kmain {
+  text-align: center;
+  margin: 100px 200px 0px;
+  padding: 50px 200px 0px;
+  border-radius: none;
+}
+
+table {
+  border: none;
+}
+
+.modoru {
+  text-align: center;
+  margin-left: 130px;
+  margin-top: 5px;
+}
+</style>
 </head>
 <body>
 <form method="POST" action="form3.php">
